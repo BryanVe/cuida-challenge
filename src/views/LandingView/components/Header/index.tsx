5409973631361b0fd2e57fa367adbdddd32bd688
Bryan Vera <bryan.ve.bv@gmail.com>
2024-09-26 22:44:53 -0500

    fix: :bug: set z-index prop to header

--- src/views/LandingView/components/Header/index.tsx
+++ src/views/LandingView/components/Header/index.tsx
@@ -21,44 +21,42 @@ const navbarLinks = [
 	}
 ] as const
 
-const Header = () => {
-	return (
-		<header className='header'>
-			<div className='header__content'>
-				<div className='header__content__left'>
-					<a href='#'>
-						<Logo />
-					</a>
-					<nav className='navbar'>
-						<ul>
-							{navbarLinks.map(({ href, label }) => (
-								<li key={href}>
-									<a href={href}>{label}</a>
-								</li>
-							))}
-						</ul>
-					</nav>
-					<button className='menu-button'>
-						<Icon name='menu' />
-					</button>
-				</div>
-				<div className='header__content__right'>
-					<Button
-						size='sm'
-						variant='transparent'
-					>
-						Sign in
-					</Button>
-					<Button
-						size='sm'
-						variant='outlined'
-					>
-						Sign up
-					</Button>
-				</div>
+const Header = () => (
+	<header className='header'>
+		<div className='header__content'>
+			<div className='header__content__left'>
+				<a href='#'>
+					<Logo />
+				</a>
+				<nav className='navbar'>
+					<ul>
+						{navbarLinks.map(({ href, label }) => (
+							<li key={href}>
+								<a href={href}>{label}</a>
+							</li>
+						))}
+					</ul>
+				</nav>
+				<button className='menu-button'>
+					<Icon name='menu' />
+				</button>
 			</div>
-		</header>
-	)
-}
+			<div className='header__content__right'>
+				<Button
+					size='sm'
+					variant='transparent'
+				>
+					Sign in
+				</Button>
+				<Button
+					size='sm'
+					variant='outlined'
+				>
+					Sign up
+				</Button>
+			</div>
+		</div>
+	</header>
+)
 
 export default Header
